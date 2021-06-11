618953b1897a249296874a0dee8e8f8c25ed76af
wangchenyu <wangcy@goisan.cn>
2021-06-11 09:59:38 +0800

    init

---
+++ src/main/java/com/kaltsit/FileCopier.java
@@ -0,0 +1,68 @@
+package com.kaltsit;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * 简易补丁打包工具
+ * @Author wangcy
+ * @Date 2021/4/12 10:22
+ */
+public class FileCopier {
+
+    static final File dir = new File("C:\\_git_repo\\3.2_Gemini_slszyzz");
+    static final File targetDir = new File("C:\\Users\\EDZ\\Desktop\\3.2_Gemini_slszyzz");
+
+    public static void main(String[] args) throws Exception {
+        InputStream is = ClassLoader.getSystemResourceAsStream("files.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        while((line = reader.readLine()) != null) {
+            if(isSourceFile(line)) {
+                String path = line.replace("src/main/app", "").replace("src/main/business", "")
+                        .replace("src/main/core", "").replace("src/main/resourcelibrary", "");
+                File source = new File(dir, "/target/classes/" + path.replace(".java", ".class"));
+                File target = new File(targetDir, "/WEB-INF/classes/" + path.replace(".java", ".class"));
+                if(!target.getParentFile().exists()) {
+                    target.getParentFile().mkdirs();
+                }
+                Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }else if(isMapperFile(line)) {
+                String path = line.replace("src/main/resources", "");
+                File source = new File(dir, "/target/classes" + path);
+                File target = new File(targetDir, "/WEB-INF/classes/" + path);
+                if(!target.getParentFile().exists()) {
+                    target.getParentFile().mkdirs();
+                }
+                Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }else if(isWebappFile(line)) {
+                File source = new File(dir, line);
+                File target = new File(targetDir, line.replace("src/main/webapp", ""));
+                if(!target.getParentFile().exists()) {
+                    target.getParentFile().mkdirs();
+                }
+                Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }else {
+                System.out.println("不识别的路径：" + line);
+            }
+        }
+    }
+
+    static boolean isSourceFile(String str) {
+        return str.contains("src/main/app") || str.contains("src/main/business") ||
+                str.contains("src/main/core") || str.contains("src/main/resourcelibrary");
+    }
+
+    static boolean isMapperFile(String str) {
+        return str.contains("src/main/resources/mapper");
+    }
+
+    static boolean isWebappFile(String str) {
+        return str.contains("src/main/webapp");
+    }
+
+}
